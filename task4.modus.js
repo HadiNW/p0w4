13013da2660fd6fa9e3b7b92ef5e7f4d0cd928e7
Hadi NW <hadinw@Hadis-MacBook-Pro.local>
2018-08-19 21:42:17 +0700

    4

--- task4.modus.js
+++ task4.modus.js
@@ -1,51 +1,48 @@
 function cariModus(arr) {
-  var jumlah=[]
-  var modus=arr[0];
-  for(var i=0;i<arr.length;i++){
-    var jumlahTemp=0;
-    for(var j=0;j<arr.length;j++){
-	if(arr[i]===arr[j]){
-           jumlahTemp=jumlahTemp+1;
+  //start
+  var modus = arr[0];
+  var temp = []
+  
+  for(var i = 0;i < arr.length; i++){
+    var temp2 = 0;
+    for(var j = 0; j < arr.length; j++){
+	if(arr[i] === arr[j]){
+           temp2++;
         }
      }
-    jumlah.push(jumlahTemp);
+    temp.push(temp2);
 
   }
-
-
-   //cek sama
-  var sama=false;
-  for(var i=1;i<jumlah.length;i++){
-	if(jumlah[0]===jumlah[i]){
-	sama=true;
-        }
-        else {
-         sama=false;
-         break;
-        }
+  // check is same?
+  var isSame = false;
+  for(var i = 1; i < temp.length; i++){
+    if(temp[0] === temp[i]){
+      isSame = true;
+    } else {
+      isSame = false;
+      break;
+    }
   }
-  if(sama===true){
+  if(isSame === true){
     return -1;
-  }
-
-  //indeks nilai maksimal
-  console.log(jumlah)
-  var maks=jumlah[0];
-  var index=0;
-  for (var i = 1; i < jumlah.length; i++) {
-    if(jumlah[i]>maks){
-      console.log(jumlah[i], maks)
-     maks=jumlah[i]
-      index=i;
+  }  
+ 
+  var count = 0;
+  var max = temp[0];
+  for (var i = 1; i < temp.length; i++) {
+    if(temp[i] > max){
+      max = temp[i]
+      count = i;
     }
   }
 
-return arr[index];
+  return arr[count];
+
 }
 
 // TEST CASES
- console.log(cariModus([10, 4, 5, 2, 4])); // 4
-// console.log(cariModus([5, 10, 10, 6, 5])); // 5
-// console.log(cariModus([10, 3, 1, 2, 5])); // -1
-// console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
-// console.log(cariModus([7, 7, 7, 7, 7])); // -1
+console.log(cariModus([10, 4, 5, 2, 4])); // 4
+console.log(cariModus([5, 10, 10, 6, 5])); // 5
+console.log(cariModus([10, 3, 1, 2, 5])); // -1
+console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+console.log(cariModus([7, 7, 7, 7, 7])); // -1
